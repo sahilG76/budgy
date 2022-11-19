f4470192889f46c56fb31110336d549d9b79be89
Darren Wang <rardenawng@gmail.com>
2022-11-19 14:33:28 -0500

    feature: delete from shopping cart

--- amazon_click.js
+++ amazon_click.js
@@ -1,15 +1,29 @@
 
 var AMAZON_URL = "https://www.amazon.com";
-alert("hi");
+var AMAZON_SHOPPING_CART = "https://www.amazon.com/gp/cart/view.html?ref_=nav_cart";
 if((AMAZON_URL == window.location.origin) && 
     (document.getElementById('add-to-cart-button') != null)) {
         $("#add-to-cart-button").click(function(){
             addToCart();
         });
+} else if(AMAZON_SHOPPING_CART == window.location) {
+    
+    $(".sc-action-delete").click(function(){
+        var product_name = $(this).children().children().attr('aria-label').substring(7).trim();
+        chrome.storage.local.get(['shopping_cart', 'balance'],function(result){
+            var cart = result.shopping_cart;
+            let amount = cart[product_name].quantity;
+            let cost_per_product = cart[product_name].price;
+            let total_cost = amount * cost_per_product;
+            delete cart[product_name];
+            chrome.storage.local.set({'shopping_cart':cart});
+            chrome.storage.local.set({'balance': result.balance + total_cost});
+        });
+    });
 }
 
 function addToCart(){
-    let product_name = $("#productTitle").text();
+    let product_name = $("#productTitle").text().trim();
     let product_url = window.location.href;
     let img_src = $("#imgTagWrapperId").children().attr('src');
     // get price
@@ -36,6 +50,10 @@ function addToCart(){
             alert("You have gone under your budget! Please remove something from your cart!");
         }
     });
+}
+
+function removeFromCart(){
+
 }
 
 function js_get_scrapes() {
